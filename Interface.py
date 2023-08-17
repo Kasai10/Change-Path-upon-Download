135c26a439cebb3f725b5e2a17089fd189b02cf2
Kasai10 <118512029+Kasai10@users.noreply.github.com>
2023-08-17 11:00:06 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,12 +1,20 @@
 import customtkinter as ctk
 import Logic
 import pandas as pd
+import os
+
+# Get the directory of the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 def prev_paths():
-    with open("paths.txt", "r") as file:
+    # Use the absolute path to the file
+    paths_file = os.path.join(script_dir, "paths.txt")
+    
+    with open(paths_file, "r") as file:
         paths = file.readlines()
         paths = [path.strip() for path in paths]
         return paths
+
 
 def updateEntry(value):
     entry_field.delete(0, ctk.END)
@@ -20,7 +28,7 @@ entry_field.pack(fill='both', expand=True)
 
 options = prev_paths()
 
-button = ctk.CTkButton(window, width=300, text='Save', command= lambda: Logic.logic(entry_field.get(), options))
+button = ctk.CTkButton(window, width=300, text='Save', command= lambda: Logic.logic(entry_field.get(), options, script_dir))
 button.pack(fill='both', expand=True)
 
 
